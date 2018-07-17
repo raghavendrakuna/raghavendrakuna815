d1325a4168853117d81323e1bf24518c9abb7199
Training_B8C.07.19 <Training_B8C.07.19@BDC8-DX-DR72T72.dir.svc.accenture.com>
2018-07-17 12:42:42 +0530

    Commited

---
+++ mission/src/test/java/mission/ProjectNew.java
@@ -0,0 +1,29 @@
+package mission;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ProjectNew {
+	@Test
+	public void testlogin()
+	{
+		System.setProperty("WebDriver.chrome.driver","C:\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get("https://github.com/login");
+		driver.findElement(By.id("login_field")).sendKeys("raghavendrakuna");
+		driver.findElement(By.id("password")).sendKeys("R@lkm815");
+		driver.findElement(By.name("commit")).click();
+		String title=driver.getTitle();
+		System.out.println(title);
+		
+		Assert.assertEquals(title,"sign in to Github .  Github");
+		
+		System.out.println("Continue to test....");
+	}
+}
+
+
